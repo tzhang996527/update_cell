27cb38d98f894734bf7f436e415f2e7f2c398146
tzhang996527@hotmail.com <tzhang996527@hotmail.com>
2019-11-10 13:34:03 +0800

    test

---
+++ src/test/java/com/excel/AppTest.java
@@ -0,0 +1,22 @@
+package com.excel;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        String[] a = new String[]{"a"};
+        App.main(a);
+    }
+}
